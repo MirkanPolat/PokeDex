2d4916ccf44223a9691e1df40add75e7fd3656d7
MirkanPolat <Mirkanpolat@outlook.de>
2025-04-07 23:33:00 +0200

    fixing some problems on the first 20 pokemons

--- script.js
+++ script.js
@@ -1,35 +1,76 @@
 let allPokemon = [];
 
-function init(){
-    getData();
+function init() {
+  getData();
 }
 
-async function getData(){
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=0#`;
-    let response = await fetch(url);
-    let data = await response.json();
-    renderCards(data);
+async function getData() {
+  for (let i = 1; i <= 20; i++) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`; // holt sich die Daten von der API
+    let response = await fetch(url); // macht eine Anfrage an die API
+    let pokemon = await response.json(); // wartet auf die Antwort der API
+    allPokemon.push(pokemon); // fügt die Daten in das Array ein
+  }
+  renderCardDiv(allPokemon);
+}
+function renderCardDiv(allPokemon) {
+  let cardContainer = document.getElementById("allPokemons"); // holt sich die div
+  cardContainer.innerHTML = "";
+
+  for (let i = 0; i < allPokemon.length; i++) {
+    // geht durch das Array
+    let element = allPokemon[i]; // holt sich das Element
+    cardContainer.innerHTML += CardInnerDiv(element, element.types); // übergibt das types-Array
+  }
 }
 
-function renderCards(data){
-    let cardContainer = document.getElementById("allPokemons");
-    cardContainer.innerHTML = "";
-    allPokemon = data.results;
-    console.log(allPokemon);
+function CardInnerDiv(element, types) {
+  // Typen als HTML-Elemente generieren
+  let typesHTML = "";
+  for (let i = 0; i < types.length; i++) {
+    typesHTML += `<div class="type">${types[i].type.name}</div>`;
+  }
 
-    for (let i = 0; i < allPokemon.length; i++) {
-        cardContainer.innerHTML += CardInnerDiv(allPokemon[i]);
-    }
-    
-
-}
-
-function CardInnerDiv(pokemon){
-    return `
-        <div class="card">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png" alt="${pokemon.name}">
-            <h3>${pokemon.name}</h3>
-            <button onclick="showDetails('${pokemon.url}')">Details</button>
-        </div>
+  return `
+      <div class="card">
+          <img src="${element.sprites.front_default}" alt="${element.name}">
+          <h3>${element.name}</h3>
+          <div class="types-container">
+              ${typesHTML} 
+          </div>
+          <p>ID: ${element.id}</p>
+          <button onclick="showDetails('${element.name}')">Details</button>
+      </div>
     `;
 }
+/* function renderCardDiv(allPokemon) {
+  let cardContainer = document.getElementById("allPokemons"); // holt sich die div
+  cardContainer.innerHTML = "";
+
+  for (let i = 0; i < allPokemon.length; i++) { // geht durch das Array
+    let element = allPokemon[i]; // holt sich das Element
+    let typeText = ""; // erstellt eine leere Variable für den Type
+
+    for (let j = 0; j < element.types.length; j++) { // geht durch die Types
+      typeText += element.types[j].type.name; // holt sich den Type
+  
+      if (j < element.types.length - 1) { // wenn es nicht der letzte Type ist
+        typeText += ", "; // fügt ein Komma hinzu
+      }
+    }
+    cardContainer.innerHTML += CardInnerDiv(element, typeText);
+  }
+}
+
+function CardInnerDiv(element, typeText) {
+    return `
+      <div class="card">
+          <img src="${element.sprites.front_default}" alt="${element.name}">
+          <h3>${element.name}</h3>
+          <p><b>Type:</b> ${typeText}</p>
+          <p>ID: ${element.id}</p>
+          <button onclick="showDetails('${element.name}')">Details</button>
+      </div>
+    `;
+}
+ */
