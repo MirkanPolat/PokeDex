b6d653814b65c5241ef3f9fba5590757bd9b677e
MirkanPolat <Mirkanpolat@outlook.de>
2025-04-07 17:29:02 +0200

    adding first 20 Pokemons to the main page

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+let allPokemon = [];
+
 function init(){
-
+    getData();
 }
 
-function getData(){
-    let url = ``
+async function getData(){
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=0#`;
+    let response = await fetch(url);
+    let data = await response.json();
+    renderCards(data);
 }
 
-function renderCards(){
+function renderCards(data){
+    let cardContainer = document.getElementById("allPokemons");
+    cardContainer.innerHTML = "";
+    allPokemon = data.results;
+    console.log(allPokemon);
+
+    for (let i = 0; i < allPokemon.length; i++) {
+        cardContainer.innerHTML += CardInnerDiv(allPokemon[i]);
+    }
     
 
+}
+
+function CardInnerDiv(pokemon){
+    return `
+        <div class="card">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png" alt="${pokemon.name}">
+            <h3>${pokemon.name}</h3>
+            <button onclick="showDetails('${pokemon.url}')">Details</button>
+        </div>
+    `;
 }
